7b464ee0bda213d27a70a1ed6b610e547200cbbc
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-02-03 21:00:05 -0500

    cleanuP

--- main.go
+++ main.go
@@ -13,6 +13,10 @@ import (
 	"github.com/op/go-logging"
 )
 
+const (
+	projectName = "hyphenated-helper"
+)
+
 var (
 	logger       *logging.Logger
 	discord      *discordgo.Session
@@ -23,7 +27,7 @@ var (
 func main() {
 	// Initialize logging using the "go-logging" library
 	// http://godoc.org/github.com/op/go-logging#Formatter
-	logger = logging.MustGetLogger("hanabi-live")
+	logger = logging.MustGetLogger(projectName)
 	loggingBackend := logging.NewLogBackend(os.Stdout, "", 0)
 	logFormat := logging.MustStringFormatter( // https://golang.org/pkg/time/#Time.Format
 		`%{time:Mon Jan 02 15:04:05 MST 2006} - %{level:.4s} - %{shortfile} - %{message}`,
@@ -85,7 +89,7 @@ func main() {
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
 	<-sc
 
-	// Cleanly close down the Discord session.
+	// Cleanly close down the Discord session
 	discord.Close()
 }
 
@@ -113,6 +117,10 @@ func discordMessageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	logger.Info("[#" + channel.Name + "] " +
 		"<" + m.Author.Username + "#" + m.Author.Discriminator + "> " + m.Content)
 
+	discordCheckCommand(m)
+}
+
+func discordCheckCommand(m *discordgo.MessageCreate) {
 	args := strings.Split(m.Content, " ")
 	command := args[0]
 
@@ -121,20 +129,15 @@ func discordMessageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	if !strings.HasPrefix(command, "!") {
 		return
 	}
+
 	command = strings.TrimPrefix(command, "!")
 	command = strings.ToLower(command) // Commands are case-insensitive
 
 	if command == "badquestion" {
 		msg := "Your question is not specific enough. In order to properly answer it, we need to know the amount of players in the game, all of the cards in all of the hands, the amount of current clues, and so forth. Please type out a full Alice and Bob story in the style of the reference document. (e.g. <https://github.com/Zamiell/hanabi-conventions/blob/master/Reference.md#the-reverse-finesse>)"
 		discordSend(m.ChannelID, msg)
-	} else if command == "badhere" {
-		msg := "It looks like there are already some Hyphen-ated members playing in one of the voice channels. Did you already ask to join them in their next game? Unless there is a specific reason (e.g. experts playing a variant that is too difficult for a beginner), then you should make an effort to join existing players **before** pinging the rest of the server. The \"/here\" command is generally only used when there is no-one around and playing a game already."
-		discordSend(m.ChannelID, msg)
 	} else if command == "wrongchannel" {
 		msg := "It looks like you are asking a question about the Hyphen-ated conventions or the Hyphen-ated group. Please put all such questions in the #questions-and-help channel, as that's what it is for."
-		discordSend(m.ChannelID, msg)
-	} else if command == "2pquestion" {
-		msg := "This channel is mostly for asking clarification questions about the Hyphen-ated conventions. In 2-player games, this doesn't really apply, because you can just play with your teammate in the exact way that you want without having to ask anybody else. Furthermore, the Hyphen-ated group typically only plays 3-6 player games, as 2-player games are considered to be much more trivial. Please ask 2-player questions sparingly."
 		discordSend(m.ChannelID, msg)
 	}
 }
